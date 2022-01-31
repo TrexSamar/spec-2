043e3721b6420ed958953c5696e94a8410a2c3c3
TrexSamar <75522798+TrexSamar@users.noreply.github.com>
2022-01-31 19:46:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import DrawerNavigator from './navigation/DrawerNavigator';
+
+ 
+
+export default function App() { return ( <NavigationContainer> <DrawerNavigator/> </NavigationContainer> ); }
---
+++ screens/CreatePost.js
@@ -0,0 +1,5 @@
+import * as React from "react"
+import {Text, View} from "react-native"
+
+export default class CreatePost extends React.Component{
+    render(){ return( <View> <Text>Create Post</Text> </View> ) } }
